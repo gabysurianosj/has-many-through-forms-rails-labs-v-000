f3f039c38c517e4ef4f3331d908f67a18a3d6b1e
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-24 15:59:03 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,8 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   validates :user, presence: true
    accepts_nested_attributes_for :user, reject_if: ->(user_data){ user_data[:username].blank? }
-   def username
+
+  def username
     self.user.username if not self.user.nil?
   end
 
